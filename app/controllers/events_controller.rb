28f512c46bb4b9cc51eb260c5c3fcc98a16367f3
krissykrissy <shinesinthesun@yahoo.com>
2018-07-20 14:34:12 -0400

    add ability to show an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,17 +5,26 @@ class EventsController < ApplicationController
 
     def create
         @event = Event.new(event_params)  
+        
         if @event.save
             flash[:notice] = "Event creatd!"
             redirect_to @event
         else
             flash.now[:alert] = "Event not created"
             render "new"
+        end
     end
 
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def index
+        @events = Event.all
+    end
     private
         def event_params
-            params.require(:event).permit(:title, :description, :start_date, :end_date. :venue, :location)
+            params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
         end
 
 end
