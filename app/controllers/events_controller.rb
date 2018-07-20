6a55fdb152a8dd39bb5de29a0872d016b1d3ebfd
krissykrissy <shinesinthesun@yahoo.com>
2018-07-20 14:47:35 -0400

    update event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,22 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def edit
+        @event = Event.find(params[:id]) 
+    end
+    
+    def update
+        @event = Event.find(params[:id])
+
+        if @event.update(event_params)
+            flash[:notice]= "Event updated!"
+        else
+            flash.now[:alert] = "Events not updated"
+            render "edit"
+        end
+    end
+    
     private
         def event_params
             params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
